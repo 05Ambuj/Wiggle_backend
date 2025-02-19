998a4000ceffc7a61b4456bd250321a25fe0232f
Ambuj Mishra <hyeambuj@gmail.com>
2025-02-20 01:30:52 +0530

    user data fetching and updating functionality added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express'
 import { isAuth } from '../middleware/isAuth.js';
-import { myProfile, userProfile } from '../controllers/userController.js';
+import { followAndUnfollowUser, myProfile, updatePassword, updateProfile, userFollowerAndFollowingData, userProfile } from '../controllers/userController.js';
+import uploadFile from '../middleware/multer.js';
 
 const router =express.Router();
 
 router.get("/myprofile",isAuth,myProfile);
 router.get("/:id",isAuth, userProfile);
- 
+router.post("/:id",isAuth, updatePassword);
+router.put("/:id",isAuth, uploadFile, updateProfile);
+router.post("/follow/:id",isAuth, followAndUnfollowUser) 
+router.get("/followdata/:id",isAuth, userFollowerAndFollowingData) 
+
 export default router;
